67751edf19cb93af1854fbe15867e920c684dbef
swpease <swpease@gmail.com>
2017-05-03 14:58:31 -0700

    ingredients and flavors models created

--- flavify/settings/base.py
+++ flavify/settings/base.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'flavors.apps.FlavorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- flavors/models.py
+++ flavors/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    base_item = models.CharField(max_length=50)
+    subcategory = models.CharField(max_length=50, blank=True)
+    tastes = models.ManyToManyField('Taste')
+
+    def __str__(self):
+        return '{} {}'.format(self.base_item, self.subcategory)
+
+class Taste(models.Model):
+    TONGUE_MAP = (
+        ('Sweet', 'Sweet'),
+        ('Salty', 'Salty'),
+        ('Sour', 'Sour'),
+        ('Bitter', 'Bitter'),
+        ('Umami', 'Umami'),
+        ('Spicy', 'Spicy'),  # mustard
+        ('Numbing', 'Numbing'),  # Szechuan peppercorns
+        ('Cooling', 'Cooling'),  # mint
+    )
+
+    mouth_taste = models.CharField(max_length=20, choices=TONGUE_MAP)
+
+    def __str__(self):
+        return self.mouth_taste
