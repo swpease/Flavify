ea7c4e527d41fc421aa1889c251f65773d20db80
swpease <swpease@gmail.com>
2017-05-03 20:41:46 -0700

    Ingredient and Taste
    ingredient and taste models created and basic view functioning. Need to
    change the schema for Ingredient, though.

--- flavors/views.py
+++ flavors/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Ingredient, Taste
+
+
+def index(request):
+    pass
+
+
+def pairings(request, ingredient):
+    ingredient_spaced = ingredient.replace('-', ' ')
+    ingredient_entry = get_object_or_404(Ingredient, base_item__exact=ingredient_spaced)
+    context = {'ingredient': ingredient_entry}
+    return render(request, 'flavors/ingredient.html', context)
