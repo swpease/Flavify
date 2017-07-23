73ffec508ba39454955ac5cdb1b7e840ff4c89f5
swpease <swpease@gmail.com>
2017-07-23 11:58:17 -0700

    table moved to index

--- flavors/urls.py
+++ flavors/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^submit-combo/', views.submit_combo, name='submit-combo'),
     url(r'^submit-ingredient/', views.submit_ingredient, name='submit-ingredient'),
     url(r'^search/', views.search, name='search'),
-    url(r'^table/(?P<ingredient>.+)/$', views.table, name='table'),
+    url(r'^table/$', views.table, name='table'),
     url(r'^(?P<ingredient>.+)/$', views.pairings, name='pairings'),
 ]
