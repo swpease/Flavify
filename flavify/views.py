afd51d21498371edefb10f87e9aa09bf77f03930
swpease <swpease@gmail.com>
2017-07-23 19:20:27 -0700

    renamed profile to submissions

--- flavify/views.py
+++ flavify/views.py
@@ -18,17 +18,15 @@ def home_files(request, filename):
 
 
 @login_required
-def profile(request):
+def submissions(request):
     """
     The user's profile page. Currently just shows two tables:
       1. All combinations they have liked/disliked/favorited/noted
       2. All of their submitted combinations
     """
     # TODO... make AJAX responsive.
-    user_combo_data = UserComboData.objects.filter(user=request.user)
     submitted_combos = Combination.objects.filter(submittor=request.user)
-    return render(request, "flavify/profile.html", {"user_combo_data": user_combo_data,
-                                                "submitted_combos": submitted_combos})
+    return render(request, "flavify/submissions.html", {"submitted_combos": submitted_combos})
 
 
 def ajax_update_ucd(request):
