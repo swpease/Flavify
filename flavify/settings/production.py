04d31caca940f150bcccf4f8d013dd0fcd670be7
swpease <swpease@gmail.com>
2017-08-14 16:27:59 -0700

    white noise

--- flavify/settings/production.py
+++ flavify/settings/production.py
@@ -3,3 +3,6 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# Heroku
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- flavify/wsgi.py
+++ flavify/wsgi.py
@@ -5,12 +5,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+
+Also see
+http://whitenoise.evans.io/en/stable/django.html
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flavify.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
