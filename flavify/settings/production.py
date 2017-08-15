bf2986b56d8c712ee2dd25e77b940e3fa3e66fec
swpease <swpease@gmail.com>
2017-08-14 19:44:22 -0700

    allowed_hosts updated

--- flavify/settings/production.py
+++ flavify/settings/production.py
@@ -6,6 +6,9 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ALLOWED_HOSTS.append('.herokuapp.com')
+ALLOWED_HOSTS.append('.flavoration.com')
+
 # Heroku
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
